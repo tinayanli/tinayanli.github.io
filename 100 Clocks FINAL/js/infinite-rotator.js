7d30e0435f5bd78932629f31c630d204b1915717
Tina Li <TL@Tinas-MBP.wireless.newschool.edu>
2017-05-11 12:06:05 -0400

    Clock Collection

---
+++ 100 Clocks FINAL/js/infinite-rotator.js
@@ -0,0 +1,40 @@
+$(window).load(function() { //start after HTML, images have loaded
+
+    var InfiniteRotator = {
+        init: function() {
+            //initial fade-in time (in milliseconds)
+            var initialFadeIn = 700;
+
+            //interval between items (in milliseconds)
+            var itemInterval = 3000;
+
+            //cross-fade time (in milliseconds)
+            var fadeTime = 2000;
+
+            //count number of items
+            var numberOfItems = $('.rotating-item').length;
+
+            //set current item
+            var currentItem = 0;
+
+            //show first item
+            $('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
+
+            //loop through the items		
+            var infiniteLoop = setInterval(function() {
+                $('.rotating-item').eq(currentItem).fadeOut(fadeTime);
+
+                if (currentItem == numberOfItems - 1) {
+                    currentItem = 0;
+                } else {
+                    currentItem++;
+                }
+                $('.rotating-item').eq(currentItem).fadeIn(fadeTime);
+
+            }, itemInterval);
+        }
+    };
+
+    InfiniteRotator.init();
+
+});
