b3b0cd9c95162c5988d8b3a25986d54a06148a05
tinayanli <liy997@newschool.edu>
2017-09-13 21:30:51 -0400

    Add files via upload

---
+++ webadvanced/Assignment_2/assets/script.js
@@ -0,0 +1,14 @@
+/*-------------- Onclick Confirmation Box --------------*/
+
+function openBox(message) {
+alert (message);
+}
+
+/*--------------- Steve Jobs Quotes ----------------*/
+
+let quotes = ['Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.', 'Have the courage to follow your heart and intuition. They somehow know what you truly want to become.', 'Quality is more important than quantity. One home run is much better than two doubles.', 'Your time is limited, so don’t waste it living someone else’s life.', 'Don’t let the noise of others’ opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.','Remembering that you are going to die is the best way I know to avoid the trap of thinking you have something to lose. You are already naked. There is no reason not to follow your heart.','If I tried my best and fail, well, I\'ve tried my best.','Sometimes when you innovate, you make mistakes. It is best to admit them quickly, and get on with improving your other innovations.','Being the richest man in the cemetery doesn’t matter to me. Going to bed at night saying we’ve done something wonderful, that’s what matters to me.', 'If today were the last day of your life, would you want to do what you are about to do today?','Stay hungry. Stay foolish.'];
+
+for (let quoteIndex = 0; quoteIndex < quotes.length; quoteIndex++){
+	console.log(quotes[quoteIndex]);
+}
+
