6573dc4685e1d839ea6c418ea76e0c45c8e1f6ce
Tina Li <TL@Tinas-MBP.fios-router.home>
2017-12-13 08:37:20 -0500

    Updated assignment 10

--- webadvanced/Assignment_10/js/main.js
+++ webadvanced/Assignment_10/js/main.js
@@ -1,3 +1,5 @@
+/*--------------------- Finland RSS Feed ---------------------*/
+
 $(document).ready(function () {
 
 	var statement = "select * from feed where url='http://www.visitfinland.com/feed/'"; // here you link the url you need
@@ -12,10 +14,31 @@ $(document).ready(function () {
 			// description.replace("src=")
 
 
-			$("#nasa").append("<h2>" + data.query.results.item[i].title + "</h2><h3>" + data.query.results.item[i].pubDate + "</h3><p>" + data.query.results.item[i].description + "</p>")
+			//$("#nasa").append("<h2>" + data.query.results.item[i].title + "</h2><h3>" + data.query.results.item[i].pubDate + "</h3><p>" + data.query.results.item[i].description + "</p>")
+
+
+			var publishDate = data.query.results.item[i].pubDate.replace("+0000", "") //replaces +0000 with no 0000
+
+
+			$("#nasa").append("<div class='finland-item'><h2>" + data.query.results.item[i].title + "</h2><h3>" + publishDate + "</h3><p>" + data.query.results.item[i].description + "</p></div>")
 
 		}
 
 	});
 
 });
+
+
+/*------------------- Yahoo Weather API ----------------------*/
+
+$(document).ready(function () {
+	var city = "Helsinki";
+	var searchtext = "select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u='f'"
+
+	$.getJSON("https://query.yahooapis.com/v1/public/yql?q=" + searchtext + "&format=json").success(function (data) {
+		console.log(data);
+		$('#temp').html("Temperature in " + city + " is " + data.query.results.channel.item.condition.temp + "°F");
+	});
+});
+
+
