4f2c6527fc2eee35029d5ba4449d05b20c7a1b05
Tina Li <TL@Tinas-MBP.fios-router.home>
2017-12-15 00:34:21 -0500

    Updated assignment 10

--- webadvanced/Assignment_10/js/main.js
+++ webadvanced/Assignment_10/js/main.js
@@ -37,8 +37,36 @@ $(document).ready(function () {
 
 	$.getJSON("https://query.yahooapis.com/v1/public/yql?q=" + searchtext + "&format=json").success(function (data) {
 		console.log(data);
-		$('#temp').html("Temperature in " + city + " is " + data.query.results.channel.item.condition.temp + "°F");
+		$('#temp').html("Current temperature in " + city + " is "+ data.query.results.channel.item.condition.temp + "°F ");
+		
+		if (data.query.results.channel.item.condition.temp > 0 && data.query.results.channel.item.condition.temp < 30) {	
+			$("#rss-section").css("background-color", "#ebfaff");
+			$(".bgimg-3").css("background-image", "url(img/stars.jpg");
+		} else if (data.query.results.channel.item.condition.temp >= 30 && data.query.results.channel.item.condition.temp < 32) {
+			$("#rss-section").css("background-color", "#e4e4e4");
+			$(".bgimg-3").css("background-image", "url(img/weather-1.jpg");
+		} else if (data.query.results.channel.item.condition.temp >= 32 && data.query.results.channel.item.condition.temp < 34) {
+			$("#rss-section").css("background-color", "#fff7f8");
+			$(".bgimg-3").css("background-image", "url(img/autumn.jpg");
+		} else if (data.query.results.channel.item.condition.temp >= 34 && data.query.results.channel.item.condition.temp < 36) {
+			$("#rss-section").css("background-color", "#eee8f3");
+			$(".bgimg-3").css("background-image", "url(img/weather-pretty.jpg"); // changes the background of Weather image 
+		} else if (data.query.results.channel.item.condition.temp >= 36 && data.query.results.channel.item.condition.temp < 38) {
+			$("#rss-section").css("background-color", "#f6f0e8");
+			$(".bgimg-3").css("background-image", "url(img/img/warm.jpg.jpg")
+		} else if (data.query.results.channel.item.condition.temp >= 38 && data.query.results.channel.item.condition.temp < 40) {
+			$("#rss-section").css("background-color", "#ffe9ec");
+			$(".bgimg-3").css("background-image", "url(img/warmwarm.jpg")
+		}
+
 	});
+	
+	
 });
 
 
+
+// THIS WORKS
+// else if (data.query.results.channel.item.condition.temp > 34 && data.query.results.channel.item.condition.temp < 36) {
+		//	$("#rss-section").css("background-color", "#fff0f2");
+		//	$(".bgimg-3").css("background-image", "url(img/weather-pretty.jpg");
